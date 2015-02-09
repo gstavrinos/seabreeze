57ddd492448b3f775b699ca693991c73abc1495d
dougocean <dougocean@26700766-15df-4065-81c8-c36b4089b01e>
2015-02-09 23:38:28 +0000

    Win64/Cywin64 lib fix plus some miscellaneous other

---
+++ branches/win64-etc/SeaBreeze/sample-code/CSharpDemo/Logger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+public class Logger
+{
+    private TextBox textBox = null;
+
+    public Logger() { }
+
+    public Logger(TextBox tb) { textBox = tb; }
+
+    public void setTextBox(TextBox tb) { textBox = tb; }
+
+    // write (not append) log to textfile
+    public void save(string pathname)
+    {
+        if (textBox != null)
+        {
+            try
+            {
+                TextWriter tw = new StreamWriter(pathname);
+                tw.WriteLine(textBox.Text);
+                tw.Close();
+            }
+            catch (Exception e)
+            {
+                display("Error saving log to {0}: {1}", pathname, e.Message);
+            }
+        }
+    }
+
+    // log to textbox and console
+    public void display(string fmt, params Object[] obj)
+    {
+        string msg = String.Format(fmt, obj) +Environment.NewLine;
+        Console.Write("DISPLAY: {0}", msg);
+        if (textBox != null)
+            textBox.AppendText(msg);
+    }
+
+    // just log to console
+    public void log(string fmt, params Object[] obj)
+    {
+        string msg = String.Format(fmt, obj) + Environment.NewLine;
+        Console.Write("LOG: {0}", msg);
+    }
+}
