c2a0d979037a50cbd15b1ff3fce8009470f1bf68
mzieg <mzieg@26700766-15df-4065-81c8-c36b4089b01e>
2015-06-10 14:46:24 +0000

    changing strtod_l to istringstream

--- trunk/SeaBreeze/src/vendors/OceanOptics/features/eeprom_slots/NonlinearityEEPROMSlotFeature.cpp
+++ trunk/SeaBreeze/src/vendors/OceanOptics/features/eeprom_slots/NonlinearityEEPROMSlotFeature.cpp
@@ -59,12 +59,8 @@ vector<double> *NonlinearityEEPROMSlotFeature::readNonlinearityCoefficients(
     int numberCoeffs;
     vector<double> *retval;
 
-    logger.debug("starting");
-
     /* Order of the polynomial is stored in slot 14 */
-    logger.debug("reading long");
     order = (int)readLong(protocol, bus, __NONLINEARITY_ORDER_SLOT);
-    logger.debug("done reading long");
 
     /* Must add one to the order to include the 0th order coefficient */
     numberCoeffs = order + 1;
@@ -74,7 +70,6 @@ vector<double> *NonlinearityEEPROMSlotFeature::readNonlinearityCoefficients(
         /* Nonlinearity coefficients are stored starting with intercept at slot 6 */
         try {
             /* This may throw a FeatureException or NumberFormatException */
-            logger.debug("calling readDouble on NLC coeff %d", i);
             (*retval)[i] = readDouble(protocol, bus, i + __NONLINEARITY_SLOT_ORDER_ZERO);
         } catch (NumberFormatException &nfe) {
             /* FIXME: If there were some sort of logging mechanism, this would
