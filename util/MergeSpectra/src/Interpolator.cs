68caf1c7b6822af54e9dd21af1bb90f11d965e12
mzieg <mzieg@26700766-15df-4065-81c8-c36b4089b01e>
2015-08-20 23:14:25 +0000

    cleanup & docs

--- util/MergeSpectra/src/Interpolator.cs
+++ util/MergeSpectra/src/Interpolator.cs
@@ -14,41 +14,40 @@ namespace MergeSpectra
         int pixels = 0;
         Logger logger = Logger.getInstance();
 
+        // We construct an Interpolator from a passed spectrum, then pre-compute
+        // anything we might want to re-use on each call to interpolate().
+        // Note that "pos" (position within the spectrum, presumed to be sequentially
+        // keyed in increasing order) is initialized to zero.
         public Interpolator(SortedDictionary<double, double> spectrumIn)
         {
             spectrum = spectrumIn;
 
             wavelengths = new List<double>(spectrum.Keys);
-            wavelengths.Sort();
+            wavelengths.Sort();     // just being thorough
             pixels = wavelengths.Count;
 
             firstWavelength = wavelengths[0];
             lastWavelength = wavelengths[pixels - 1];
         }
 
-        // NOTE: "sequential" means that it should be called with INCREASING wavelength values, which
-        // allows faster processing; random-access interpolation should be achieved through 
-        // interpolateRandom() (not currently implemented)
-        public double interpolateSequential(double wavelength)
+        // NOTE: this method will execute much more efficiently if called with 
+        // sequentially increasing wavelength values (though it should still work
+        // if used as "random access" into the spectrum, just more slowly)
+        public double interpolate(double wavelength)
         {
-            // handle corner-cases fast
-            if (wavelength <= firstWavelength)
-                return spectrum[firstWavelength];
-            if (wavelength >= lastWavelength)
-                return spectrum[lastWavelength];
-            if (spectrum.ContainsKey(wavelength))
-                return spectrum[wavelength];
+            // handle corner-cases fast (leave 'pos' where it is) - no misleadingly cute extrapolation, 
+            // just treat first and last values as infinite series
+            if (wavelength <= firstWavelength)    return spectrum[firstWavelength]; 
+            if (wavelength >= lastWavelength)     return spectrum[lastWavelength]; 
+            if (spectrum.ContainsKey(wavelength)) return spectrum[wavelength];
 
-            // this shouldn't happen if caller follows the "sequential" requirement
+            // this shouldn't happen if caller follows the "sequential" guidance (i.e.
+            // each subsequent method call is invoked with an increasing wavelength value)
             if (wavelengths[pos] > wavelength)
-            {
-                // just reset and let the caller suffer the runtime penalty
-                logger.log("WARNING: sequential calling error: wavelengths[{0}] ({1:f2}) > wavelength ({3:f2})", pos, wavelengths[pos], wavelength);
                 pos = 0;
-            }
 
-            // pos is an index SOMEWHERE below the requested wavelength
-            // advance until it is the index DIRECTLY below the requested wavelength
+            // pos should now be an index SOMEWHERE BELOW the requested wavelength;
+            // advance until it is the index DIRECTLY BELOW the requested wavelength
             while (pos < pixels - 2)
             {
                 if (wavelengths[pos + 1] > wavelength)
@@ -56,16 +55,18 @@ namespace MergeSpectra
                 pos++;
             }
 
-            // pos should now be directly below requested wavelength
+            // pos should now be DIRECTLY BELOW requested wavelength (don't worry
+            // about running off the end...handled by initial corner-case)
 
-            // lookup bracketing wavelength and intensity values
+            // lookup bracketing wavelengths
             double wl0 = wavelengths[pos];
             double wl1 = wavelengths[pos + 1];
         
+            // lookup associated bracketing intensity values
             double a0 = spectrum[wl0];
             double a1 = spectrum[wl1];
         
-            // interpolate
+            // perform linear interpolation between those two points
             return (((wavelength - wl0) / (wl1 - wl0)) * (a1 - a0)) + a0;
         }
     }
