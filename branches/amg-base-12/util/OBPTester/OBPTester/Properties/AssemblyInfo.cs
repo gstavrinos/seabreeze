bcd7af6722367977972e3a394553d9d5d52a999a
ratcrow <ratcrow@26700766-15df-4065-81c8-c36b4089b01e>
2015-11-27 22:36:34 +0000

    Creating a branch to work on issue [#12] (reviewing how the base Exchange and Transaction protocol classes are structured).

---
+++ branches/amg-base-12/util/OBPTester/OBPTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OBPTester")]
+[assembly: AssemblyDescription("Tests various Ocean Binary Protocol (OBP) messages")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ocean Optics, Inc.")]
+[assembly: AssemblyProduct("OBPTester")]
+[assembly: AssemblyCopyright("Copyright © 2015 Ocean Optics, Inc.  All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02bda1f3-2193-43e1-9475-97feb389d9fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
