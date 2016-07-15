f9b350b6fb428da77db16e22ef4ad3d8c14e6752
kengmcd <kengmcd@26700766-15df-4065-81c8-c36b4089b01e>
2016-07-15 12:23:37 +0000

    Tagging release 2.1.1 for minor bug fixes to the configuration.

---
+++ tags/dev-kit-2.1.1/STS-Dev-Kit/ActiveObject.cpp
@@ -0,0 +1,79 @@
+/***************************************************//**
+ * @file    ActiveObject.cpp
+ * @date    February 2015
+ * @author  Ocean Optics, Inc.
+ *
+ * An active object is an object that manages its own thread of execution. Tasks
+ * can be submitted for execution o this thread by deriving a "message" from the
+ * ActiveObject::Message class with an Execute method containing the work to be done.
+ * Tasks are queued to a "bounded buffer" and executed in turn. When we are finished
+ * with the active object the destructor queues a special message to end the thread
+ * enabling a clean exit.
+ *
+ *
+ * LICENSE:
+ *
+ * Dev Kit Copyright (C) 2015, Ocean Optics Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************/
+
+#include "ActiveObject.h"
+
+/* Start the queue of tasks in a separate thread. Take jobs
+* from the queue and execute them until we receive the special "done" job.
+*/
+void ActiveObject::Run() {
+    // This method is run in a thread on construction. It takes
+    // jobs from the queue and runs their Execute method
+    // until the done message is sent by the destructor.
+    MessagePtr msg;
+    do {
+        m_queue->pop_back(&msg);
+        msg->Execute();
+    } while (msg != m_done);
+}
+
+/* Add a task to the queue.
+*/
+void ActiveObject::Send(MessagePtr m) {
+    m_queue->push_front(m);
+}
+
+/* Constructor. Initialises the bounded buffer (the size, 128 is arbitrary) and
+* start the Run method in a separate thread.
+*/
+ActiveObject::ActiveObject() :
+    m_done(new Message) {
+
+    // Set up the queue of jobs and start the Run method in a thread.
+    m_queue.reset(new bounded_buffer< MessagePtr >(128));
+	m_thread.reset(new boost::thread(&ActiveObject::Run, this));
+}
+
+/* Desrtuctor. Send the "done" message to the queue and wait for the thread to complete.
+* Note/warning: this means any remaining jobs in the queue will be completed before we are allowed to exit
+* the destructor.
+*/
+ActiveObject::~ActiveObject() {
+    // Send the done message and wait for the thread to complete
+    Send(m_done);
+    m_thread->join();
+}
---
+++ tags/dev-kit-2.1.1/STS-Dev-Kit/Connection.h
@@ -0,0 +1,77 @@
+/***************************************************//**
+ * @file    Connection.h
+ * @date    February 2015
+ * @author  Ocean Optics, Inc.
+ *
+ * This class is used to contain a socket and perform some of the basic operations
+ * such as reading a command and any arguments.
+ *
+ * LICENSE:
+ *
+ * Dev Kit Copyright (C) 2015, Ocean Optics Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************/
+
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include <boost/asio.hpp>
+#include <memory>
+#include <string>
+#include <vector>
+#include <utility>
+
+class Connection {
+public:
+    /* Return the actual socket we are communicating on.
+    */
+    boost::asio::ip::tcp::socket *Socket();
+    /* Convenience method. Return true if the socket is open, false otherwise.
+    */
+    bool IsOpen();
+    /* Convenience method. Close the socket.
+    */
+    void Close();
+
+    /* Return the received command and the arguments accompanying it.
+    */
+    std::pair<unsigned short, std::string> GetRequest();
+
+    /* Convenience method. Reset the socket.
+    */
+    void Reset();
+
+    /* Set up the connection
+    */
+    Connection(boost::asio::io_service &service);
+    ~Connection();
+private:
+    boost::asio::io_service &m_service;
+    std::unique_ptr<boost::asio::ip::tcp::socket> m_socket;
+
+    // the buffer we use for the IO service communication
+    std::vector<unsigned char> m_buffer;
+    /* a request is 4 bytes long. the first 2 bytes are the command (MSB first) followed by 2 bytes
+     * specifying the size of the accompanying arguments (MSB first).
+    */
+    static const size_t ms_requestSize = 4;
+};
+#endif
