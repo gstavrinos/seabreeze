beed4028be145db041e71ac8ac599a0c975b0177
clendinning <clendinning@26700766-15df-4065-81c8-c36b4089b01e>
2017-01-25 17:50:59 +0000

    VS project that now opens a FlameX Spectrometer.

--- branches/FlameXIntegration/test/VisualStudio2015/FlameX_SeaBreeze_Command_Test/FlameX_SeaBreeze_Test.cs
+++ branches/FlameXIntegration/test/VisualStudio2015/FlameX_SeaBreeze_Command_Test/FlameX_SeaBreeze_Test.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
 
 
 
@@ -14,14 +15,40 @@ namespace FlameX_SeaBreeze_Command_Test
 {
     public partial class FlameX_SeaBreeze_Test : Form
     {
+
+        List<SeaBreezeSpectrometerClass> spectrometerList = new List<SeaBreezeSpectrometerClass>();
+
         public FlameX_SeaBreeze_Test()
         {
             InitializeComponent();
+
+            this.Text = String.Format("Flame X SeaBreeze Test (Version: {0})", Assembly.GetExecutingAssembly().GetName().Version.ToString());
         }
 
         private void buttonFindFlameX_Click(object sender, EventArgs e)
         {
-            SeaBreezeWrapper
+            int index = 0;
+            int zero_on_success = 0;
+            int errorCode = 0;
+
+            do
+            {
+                zero_on_success = SeaBreezeWrapper.seabreeze_open_spectrometer(index, ref errorCode);
+                if ((zero_on_success == 0) && (errorCode == 0))
+                {
+                    spectrometerList.Add(new SeaBreezeSpectrometerClass(index));
+                    index++;
+                }
+            } while (zero_on_success == 0);
+            
+            foreach(SeaBreezeSpectrometerClass aSpectrometer in spectrometerList)
+            {
+                string spectrometerID = aSpectrometer.GetNameAndSerialNumber();
+                if (spectrometerID.Contains("FLAMEX"))
+                    listBoxFlameXFound.Items.Add(spectrometerID);
+                else
+                    aSpectrometer.Close();
+            }
         }
     }
 }
---
+++ branches/FlameXIntegration/test/VisualStudio2015/FlameX_SeaBreeze_Command_Test/SeaBreezeSpectrometerClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlameX_SeaBreeze_Command_Test
+{
+    class SeaBreezeSpectrometerClass
+    {
+        private Int32 seaBreezeIndex = -1;
+
+        public SeaBreezeSpectrometerClass(int spectrometerIndex)
+        {
+            seaBreezeIndex = spectrometerIndex;
+        }
+
+        ~SeaBreezeSpectrometerClass()
+        {
+            Close();
+        }
+
+        public void Close()
+        {
+            if(seaBreezeIndex > 0)
+            {
+                Int32 myErrorCode = 0;
+                Int32 aLength = SeaBreezeWrapper.seabreeze_get_error_string_maximum_length();
+                byte[] myBuffer = new byte[aLength];
+
+                int totallyBogus = SeaBreezeWrapper.seabreeze_close_spectrometer(seaBreezeIndex, ref myErrorCode);
+                int bytesInBuffer = SeaBreezeWrapper.seabreeze_get_error_string(myErrorCode, ref myBuffer[0], aLength);
+                string myErrorString = "Exception closing spectrometer: " + System.Text.Encoding.Default.GetString(myBuffer);
+                seaBreezeIndex = -1;
+                if (myErrorCode != 0)
+                    throw (new Exception(myErrorString));
+            }
+        }
+
+        public string GetNameAndSerialNumber()
+        {
+            Int32 myErrorCode = 0;
+
+            Int32 errorStringLength = SeaBreezeWrapper.seabreeze_get_error_string_maximum_length();
+            byte[] errorStringBuffer = new byte[errorStringLength];
+
+            Int32 serialNumberStringLength = SeaBreezeWrapper.seabreeze_get_serial_number_max_length(seaBreezeIndex, ref myErrorCode);
+            byte[] mySerialNumberBuffer = new byte[serialNumberStringLength];
+
+            int bytesInBuffer = SeaBreezeWrapper.seabreeze_get_serial_number(seaBreezeIndex, ref myErrorCode, ref mySerialNumberBuffer[0], serialNumberStringLength);
+            if (myErrorCode != 0)
+            {
+                bytesInBuffer = SeaBreezeWrapper.seabreeze_get_error_string(myErrorCode, ref errorStringBuffer[0], errorStringLength);
+                string myErrorString = "Exception getting model description: " + System.Text.Encoding.Default.GetString(errorStringBuffer);
+                throw (new Exception(myErrorString));
+            }
+
+            Int32 modelStringLength = SeaBreezeWrapper.seabreeze_get_model_string_maximum_length();
+            byte[] myModelTypeBuffer = new byte[modelStringLength];
+
+            bytesInBuffer = SeaBreezeWrapper.seabreeze_get_model(seaBreezeIndex, ref myErrorCode, ref myModelTypeBuffer[0], modelStringLength);
+            if (myErrorCode != 0)
+            {
+                bytesInBuffer = SeaBreezeWrapper.seabreeze_get_error_string(myErrorCode, ref errorStringBuffer[0], errorStringLength);
+                string myErrorString = "Exception getting model description: " + System.Text.Encoding.Default.GetString(errorStringBuffer);
+                throw (new Exception(myErrorString));
+            }
+
+            string name = System.Text.Encoding.UTF8.GetString(myModelTypeBuffer).TrimEnd((char)0);
+            string serialNumber = System.Text.Encoding.UTF8.GetString(mySerialNumberBuffer).TrimEnd((char)0);
+            return name + " " + serialNumber;
+        }
+
+    }
+}
