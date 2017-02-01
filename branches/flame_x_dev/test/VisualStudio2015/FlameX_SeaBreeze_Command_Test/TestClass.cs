aaa53ffc662938f33031e0cba1545edbb19b29f9
clendinning <clendinning@26700766-15df-4065-81c8-c36b4089b01e>
2017-02-01 21:54:17 +0000

    dark indicies test in windows version.

--- branches/flame_x_dev/test/VisualStudio2015/FlameX_SeaBreeze_Command_Test/TestClass.cs
+++ branches/flame_x_dev/test/VisualStudio2015/FlameX_SeaBreeze_Command_Test/TestClass.cs
@@ -11,6 +11,8 @@ namespace FlameX_SeaBreeze_Command_Test
     {
         const int SEABREEZE_ID = 0;
         ListBox loggingListBox;
+        private static int errorStringLength = SeaBreezeWrapper.seabreeze_get_error_string_maximum_length();
+        private byte[] errorMessage = new byte[errorStringLength];
 
         public TestClass(ListBox listBoxForLogging)
         {
@@ -39,6 +41,18 @@ namespace FlameX_SeaBreeze_Command_Test
             {
                 loggingListBox.Items.Add(message);
             }
+        }
+
+        public string GetErrorDescription(int errorCode)
+        {
+            string description = "Could not retrieve the error description";
+            int bogus = 0;
+
+
+
+            int stringlength = SeaBreezeWrapper.seabreeze_get_error_string(errorCode, ref errorMessage[0], errorStringLength);
+
+            return errorMessage.ToString();
         }
 
         public void GetSerialNumber()
