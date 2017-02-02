024ab786d53cf045d4235be542aa5e968c6ca292
clendinning <clendinning@26700766-15df-4065-81c8-c36b4089b01e>
2017-02-02 14:56:07 +0000

    revert to pre-c++11 for customer still on CentOS 6.6

---
+++ branches/seabreeze_pre_cpp11/test/VisualStudio2015/FlameX_SeaBreeze_Command_Test/TestClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlameX_SeaBreeze_Command_Test
+{
+    class TestClass
+    {
+        const int SEABREEZE_ID = 0;
+        ListBox loggingListBox;
+
+        public TestClass(ListBox listBoxForLogging)
+        {
+            loggingListBox = listBoxForLogging;
+            GetSerialNumber();
+        }
+
+        ~TestClass()
+        {
+
+        }
+
+        public virtual void RunSpectrometerSpecificTests()
+        {
+            Log("No more tests are implemented.");
+        }
+
+        public void RunCommonTests()
+        {
+            Log("Running Common Tests.");
+        }
+
+        public void Log(string message)
+        {
+            if (loggingListBox != null)
+            {
+                loggingListBox.Items.Add(message);
+            }
+        }
+
+        public void GetSerialNumber()
+        {
+
+            Int32 myErrorCode = 0;
+
+            Int32 errorStringLength = SeaBreezeWrapper.seabreeze_get_error_string_maximum_length();
+            byte[] errorStringBuffer = new byte[errorStringLength];
+
+            Int32 serialNumberStringLength = SeaBreezeWrapper.seabreeze_get_serial_number_max_length(SEABREEZE_ID, ref myErrorCode);
+            byte[] mySerialNumberBuffer = new byte[serialNumberStringLength];
+
+            int bytesInBuffer = SeaBreezeWrapper.seabreeze_get_serial_number(SEABREEZE_ID, ref myErrorCode, ref mySerialNumberBuffer[0], serialNumberStringLength);
+            if (myErrorCode != 0)
+            {
+                bytesInBuffer = SeaBreezeWrapper.seabreeze_get_error_string(myErrorCode, ref errorStringBuffer[0], errorStringLength);
+                string myErrorString = "Exception getting model description: " + System.Text.Encoding.Default.GetString(errorStringBuffer);
+                throw (new Exception(myErrorString));
+            }
+            string mySerialNumber = System.Text.Encoding.UTF8.GetString(mySerialNumberBuffer).TrimEnd((char)0);
+            Log("Serial Number: " + mySerialNumber);
+        }
+
+    }
+}
